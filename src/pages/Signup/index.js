d6789ca2d8e603d7cf07d4091b08aa9c61afcffc
Adriano <adriano_alves_2008@hotmail.com>
2022-08-28 10:53:17 -0300

    página de cadastro

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../../assets/logo.png'
 
 function SignUp() {
-  return <div>Tela de cadastro</div>
+  const [nome, setNome] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSubmit(e) {
+    e.preventDefault()
+    alert('clicou')
+  }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="login-area">
+          <img src={logo} alt="Logo do sistema" />
+        </div>
+        <form onSubmit={handleSubmit}>
+          <h1>Cadastrar uma conta</h1>
+          <input
+            type="text"
+            placeholder="Seu nome"
+            value={nome}
+            onChange={e => setNome(e.target.value)}
+          />
+          <input
+            type="text"
+            placeholder="email@email.com"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="*******"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <button type="submit">Cadastrar</button>
+        </form>
+
+        <Link to="/">Já tem uma conta? Entre</Link>
+      </div>
+    </div>
+  )
 }
 
 export default SignUp
