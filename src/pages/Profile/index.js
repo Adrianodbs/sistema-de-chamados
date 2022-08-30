3c40649d830e623121fa0e5836546ad75383d409
Adriano <adriano_alves_2008@hotmail.com>
2022-08-30 09:48:30 -0300

    title

---
+++ src/pages/Profile/index.js
@@ -0,0 +1,21 @@
+import './profile.css'
+import Header from '../../components/Header'
+import Title from '../../components/Title'
+
+import { FiSettings } from 'react-icons/fi'
+
+function Profile() {
+  return (
+    <div>
+      <Header />
+
+      <div className="content">
+        <Title name="Meu perfil">
+          <FiSettings size={25} />
+        </Title>
+      </div>
+    </div>
+  )
+}
+
+export default Profile
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Route from './Route'
 import SignIn from '../pages/Signin'
 import SignUp from '../pages/Signup'
 import Dashboard from '../pages/Dashboard'
+import Profile from '../pages/Profile'
 
 export default function Routes() {
   return (
@@ -12,6 +13,7 @@ export default function Routes() {
       <Route exact path="/register" component={SignUp} />
 
       <Route exact path="/dashboard" component={Dashboard} isPrivate />
+      <Route exact path="/profile" component={Profile} isPrivate />
     </Switch>
   )
 }
