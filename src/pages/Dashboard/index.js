d0643fc7f4b0e75484c069ecc3d9c238d5c0fcad
Adriano <adriano_alves_2008@hotmail.com>
2022-09-01 11:17:21 -0300

    Criando tabela de chamados

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,10 +5,10 @@ import Header from '../../components/Header'
 import Title from '../../components/Title'
 
 import './dashboard.css'
-import { FiMessageSquare, FiPlus } from 'react-icons/fi'
+import { FiMessageSquare, FiPlus, FiSearch, FiEdit2 } from 'react-icons/fi'
 
 function Dashboard() {
-  const [chamados, setChamados] = useState([])
+  const [chamados, setChamados] = useState([1])
   return (
     <div>
       <Header />
@@ -31,6 +31,47 @@ function Dashboard() {
               <FiPlus size={25} color="#fff" />
               Novo chamado
             </Link>
+
+            <table>
+              <thead>
+                <tr>
+                  <th scope="col">Cliente</th>
+                  <th scope="col">Assunto</th>
+                  <th scope="col">Status</th>
+                  <th scope="col">Cadastrado em</th>
+                  <th scope="col">#</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td data-label="Cliente">Sujeito</td>
+                  <td data-label="Assunto">Suporte</td>
+                  <td data-label="Status">
+                    <span
+                      className="badge"
+                      style={{ backgroundColor: '#5cb85c' }}
+                    >
+                      Em aberto
+                    </span>
+                  </td>
+                  <td data-label="Cadastrado">01/09/2022</td>
+                  <td data-label="#">
+                    <button
+                      className="action"
+                      style={{ backgroundColor: '#3583f6' }}
+                    >
+                      <FiSearch color="#fff" size={17} />
+                    </button>
+                    <button
+                      className="action"
+                      style={{ backgroundColor: '#f6a935' }}
+                    >
+                      <FiEdit2 color="#fff" size={17} />
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
           </>
         )}
       </div>
