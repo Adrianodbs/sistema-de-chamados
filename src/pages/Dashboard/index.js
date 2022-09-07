0f4ee064540631ca6f4c98e16268439e5f7c12ed
Adriano <adriano_alves_2008@hotmail.com>
2022-09-07 14:20:46 -0300

    buscando chamados pt2

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -72,6 +72,35 @@ function Dashboard() {
 
     setLoadingMore(false)
   }
+
+  async function handleMore() {
+    setLoadingMore(true)
+    await listRef
+      .startAfter(lastDocs)
+      .limit(5)
+      .get()
+      .then(snapshot => {
+        updateState(snapshot)
+      })
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <Header />
+        <div className="content">
+          <Title name="Atendimentos">
+            <FiMessageSquare size={25} />
+          </Title>
+
+          <div className="container dashboard">
+            <span>Buscando chamados ...</span>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <Header />
@@ -106,35 +135,54 @@ function Dashboard() {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td data-label="Cliente">Sujeito</td>
-                  <td data-label="Assunto">Suporte</td>
-                  <td data-label="Status">
-                    <span
-                      className="badge"
-                      style={{ backgroundColor: '#5cb85c' }}
-                    >
-                      Em aberto
-                    </span>
-                  </td>
-                  <td data-label="Cadastrado">01/09/2022</td>
-                  <td data-label="#">
-                    <button
-                      className="action"
-                      style={{ backgroundColor: '#3583f6' }}
-                    >
-                      <FiSearch color="#fff" size={17} />
-                    </button>
-                    <button
-                      className="action"
-                      style={{ backgroundColor: '#f6a935' }}
-                    >
-                      <FiEdit2 color="#fff" size={17} />
-                    </button>
-                  </td>
-                </tr>
+                {chamados.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td data-label="Cliente">{item.cliente}</td>
+                      <td data-label="Assunto">{item.assunto}</td>
+                      <td data-label="Status">
+                        <span
+                          className="badge"
+                          style={{
+                            backgroundColor:
+                              item.status === 'Aberto' ? '#5cb85c' : '#999'
+                          }}
+                        >
+                          {item.status}
+                        </span>
+                      </td>
+                      <td data-label="Cadastrado">{item.createdFormated}</td>
+                      <td data-label="#">
+                        <button
+                          className="action"
+                          style={{ backgroundColor: '#3583f6' }}
+                        >
+                          <FiSearch color="#fff" size={17} />
+                        </button>
+                        <button
+                          className="action"
+                          style={{ backgroundColor: '#f6a935' }}
+                        >
+                          <FiEdit2 color="#fff" size={17} />
+                        </button>
+                      </td>
+                    </tr>
+                  )
+                })}
               </tbody>
             </table>
+
+            {loadingMore && (
+              <h3 style={{ textAlign: 'center', marginTop: 15 }}>
+                Buscado chamados...
+              </h3>
+            )}
+
+            {!loadingMore && !isEmpty && (
+              <button className="btn-more" onClick={handleMore}>
+                Buscar mais
+              </button>
+            )}
           </>
         )}
       </div>
